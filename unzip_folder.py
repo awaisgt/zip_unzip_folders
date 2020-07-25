40532fe185bd5e57df25b1c6e5647c82351288cd
awaisgt <39981782+awaisgt@users.noreply.github.com>
2020-07-26 00:52:26 +0500

    This code unzips a file to a folder.
    This code unzips a file to a folder.

---
+++ unzip_folder.py
@@ -0,0 +1,4 @@
+import shutil
+folder_path = "ABC" # here you will provide the folder path in which you want to unzip the file.
+zipped_filename = "result_file" # here you will provide the path for the zip file which you want to unzip
+shutil.make_archive(zipped_filename,folder_path,zip)
